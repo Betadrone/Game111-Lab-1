c354b1782c9486436d9999db5695b8517191a7ae
Betadrone <Betadrone596@gmal.com>
2022-10-17 14:17:43 -0400

    new

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var jackson = function(){
-    for(i = 0; i < 10; i++){
-        if(i % 2)
-        console.log("Micheal")
-        else
-        console.log("Jackson")
-    }
-}
-jackson();
+// var jackson = function(){
+//     for(i = 0; i < 10; i++){
+//         if(i % 2)
+//         console.log("Micheal")
+//         else
+//         console.log("Jackson")
+//     }
+// }
+// jackson();
+
